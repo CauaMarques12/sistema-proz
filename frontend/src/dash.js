a8d1e7e5690e896c2ef2c373888b2c0f5833f4da
Cauã <caua.marques.2006@gmail.com>
2024-04-02 17:13:33 -0300

    Initial Commit Dash: Creating file and starting dashboard project
    Iniciando projeto de dashboard nada no layout foi feito, apenas conectei a biblioteca chart.js e fiz alguns testes

---
+++ frontend/src/dash.js
@@ -0,0 +1,36 @@
+const ctx = document.getElementById('dash1');
+
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN'],
+    datasets: [{
+      label: 'Numero de Acessos',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1,
+      backgroundColor: 'rgb(255, 99, 132 )'
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    animations: {
+        tension: {
+          duration: 1000,
+          easing: 'linear',
+          from: 1,
+          to: 0,
+          loop: true
+        }
+      },
+      scales: {
+        y: { // defining min and max so hiding the dataset does not change scale range
+          min: 0,
+          max: 100
+        }
+      }
+    }
+});
