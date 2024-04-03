b2183a584b7f3854a72fc40fffacf9c1b4c505ae
Cauã <caua.marques.2006@gmail.com>
2024-04-02 21:27:34 -0300

    style: defined layout for dashboard
    Defini as limitações do layout, ficou pendente encaixar os gráficos  nos containers e puxar as informações do back-end

--- frontend/src/dash.js
+++ frontend/src/dash.js
@@ -5,17 +5,17 @@ document.getElementById('open_btn').addEventListener('click', function () {
 
 
 
-const ctx = document.getElementById('dash1');
+const ctx = document.getElementById('dash');
 
 new Chart(ctx, {
-  type: 'line',
+  type: 'pie',
   data: {
     labels: ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN'],
     datasets: [{
-      label: 'Numero de Acessos',
-      data: [12, 19, 3, 5, 2, 3],
+      label: ['Numero de Acessos', 'Julio gay,'],
+      data: [28, 19, 25, 32, 78, 43],
       borderWidth: 1,
-      backgroundColor: 'rgb(255, 99, 132 )'
+      backgroundColor: ['rgb(255, 99, 132 )', '#ff6600',]
     }]
   },
   options: {
@@ -24,13 +24,57 @@ new Chart(ctx, {
         beginAtZero: true
       }
     },
+
+    responsive: true,
+    width: 550, 
     animations: {
         tension: {
           duration: 1000,
           easing: 'linear',
           from: 1,
           to: 0,
-          loop: true
+          loop: false,
+        }
+      },
+      scales: {
+        y: { // defining min and max so hiding the dataset does not change scale range
+          min: 0,
+          max: 100
+        }
+      }
+    }
+});
+
+
+
+
+const cty = document.getElementById('dash');
+
+new Chart(cty, {
+  type: 'bar',
+  data: {
+    labels: ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN'],
+    datasets: [{
+      label: ['Numero de Acessos', 'Julio gay,'],
+      data: [28, 19, 25, 32, 78, 43],
+      borderWidth: 1,
+      backgroundColor: ['rgb(255, 99, 132 )', '#ff6600',]
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    responsive: true,
+    animations: {
+        tension: {
+          duration: 1000,
+          easing: 'linear',
+          from: 1,
+          to: 0,
+          loop: false,
         }
       },
       scales: {
