c7afc820c4ac330ececc3c6e25ff4068169f6761
vouto <vitinhodaniel887@gmail.com>
2024-04-05 17:08:23 -0300

    retorna perfil do usuário logado com base no token JWT

---
+++ backend/public/getProfile.php
@@ -0,0 +1,25 @@
+<?php
+
+require '../vendor/autoload.php';
+require 'auth.php';
+
+
+use app\database\Connection;
+
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
+
+$pdo = Connection::connect();
+
+$prepare = $pdo->prepare('select nome, email, setor, genero from clientes where email = :email');
+$prepare->execute(
+    [
+        'email' => $decoded->email,
+    ]
+);
+
+$userFound = $prepare->fetchAll();
+
+echo json_encode($userFound, JSON_PRETTY_PRINT);
+
+exit();
