d999293b464bd48af99fb046b7c7d4e145890061
Cauã <caua.marques.2006@gmail.com>
2024-04-06 18:41:56 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/CauaMarques12/sistema-proz""
    This reverts commit 78642d970ea1675f39ca1a74d0e9fa7d94b00a2e.

--- frontend/src/utils/validate-data.js
+++ frontend/src/utils/validate-data.js
@@ -8,7 +8,7 @@ export function validateData(data) {
   }
 
   const isNameOnData = data.name;
-  const isNameValid = data.name.length <= 50;
+  const isNameValid = isNameOnData ? data.name.length <= 50 : false;
   if (isNameOnData && !isNameValid) {
     setFormError("Nome deve conter somente 50 caracteres.", "name");
     return false;
@@ -30,7 +30,7 @@ export function validateData(data) {
   }
 
   const isCellOnData = data.cell;
-  const isCellValid = data.cell.length == 11;
+  const isCellValid = isCellOnData ? data.cell.length == 11: false;
   if (isCellOnData && !isCellValid) {
     setFormError("Número de telefone precisa conter 11 caracteres.", "phone");
     return false;
